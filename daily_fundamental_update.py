2c0e3208bf87cc71b8a9eb9df31f1e4e1e609a3a
Arthur Wang <Arthur@ArthurWang.net>
2020-10-06 22:43:11 -0700

    Added comments for better understanding of the code, I can't remember why I worte those in 7 minutes.

--- daily_fundamental_update.py
+++ daily_fundamental_update.py
@@ -12,6 +12,7 @@ import time
 from utilties import progressBar
 
 def get_fundamental_update(symbol):
+    #TD API limits request to 120 per second. That is maxium 2 request per second, so sleep here.
     time.sleep(0.5)
     #Define payload
     payload = {
@@ -31,38 +32,55 @@ def get_fundamental_update(symbol):
 
 
 if __name__ == '__main__':
+    #Create today's date for later file naming use.
     today = datetime.datetime.now().strftime('%Y-%m-%d')
+    #Create start time to track script running time.
     startTime = datetime.datetime.now()
+    #Set logging.
     logging.basicConfig(filename=td.REPORTS_DATA_PATH + today +'_fundamental.log', level=logging.DEBUG )
-
+    #Print some information about start of the script
     print("Script Started at {}".format(startTime.strftime("%Y/%m/%d, %H:%M:%S")))
     logging.info("Script Started at {}".format(startTime.strftime("%Y/%m/%d, %H:%M:%S")))
-
-    
+    #Read in stock list.
     stock_list = list(pd.read_csv(td.REFERENCES_DATA_PATH + 'us_exchanges_instruments.csv')['symbol'])
-    
+    #Inital storage variables
     all_fundamental = pd.DataFrame([])
-    
-    failed_list = []
+
+    """
+    This script will try to get fundamental data for 4 times. 
+    During the process, the successed stock will be removed from the list, so next try will only on stocks that failed last time.
+    After 4 tries, the remaining stocks will be write to fail list file, stamped with date and stored in Report directory.
+    """    
 
     for i in range(4):
+        #Print number of attemps.
         print('This is {} try.'.format(i))
+        #Go through list to fectch stock's fundamental info. Here we used a progress bar for better visual.
         for symbol in progressBar(stock_list, prefix='Progress:', suffix='Complete', length= 50):
+            #This will return either a dataframe contians fundamental data, or an expection object which means the request failed.
             df = get_fundamental_update(symbol)
-            if isinstance(df, pd.DataFrame):
+            #Check if df contians fundamental data or expection.
+            if isinstance(df, pd.DataFrame): #If successed
+                #Store this stock's fundamental in storage variablE.
                 all_fundamental = all_fundamental.append(df)
+                #Remove this symbol from list so it won't be request next time.
                 stock_list.remove(symbol)
+                #Put something in log.
                 logging.info(symbol + ' Completed')
             else:
+                #This stock has failed on request, log it.
                 logging.warning(symbol +' Failed because' + str(df))
     
 
-    #Now after trying for 4 times, we could write the result to file.
+    #Now after trying for 4 times, we could write the result to file. 
     all_fundamental.to_csv(td.FUNDAMENTAL_DATA_PATH + today +'.csv')
+    #Write the failed stocks into fail for record.
     with open(td.REPORTS_DATA_PATH + today + '_fundamental_failed_list.txt') as f:
         for symbol in stock_list:
             f.write("%s\n" %symbol)
     f.close()
+
+    #Print out script ending time and total execution time.
     endtime = datetime.datetime.now()
     print("Script Started at {}".format(endtime.strftime("%Y/%m/%d, %H:%M:%S")))
     logging.info("Script Started at {}".format(endtime.strftime("%Y/%m/%d, %H:%M:%S")))
